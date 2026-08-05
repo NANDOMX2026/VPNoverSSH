937bd823a6add46df5b1a02351f028cdc5c2f4eb
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 11:46:01 -0600

    FIX FINAL loadProfiles + R escudo

--- app/src/main/java/com/nandomx/v5/fragments/InicioFragment.java
+++ app/src/main/java/com/nandomx/v5/fragments/InicioFragment.java
@@ -1,10 +1,17 @@
 package com.nandomx.v5.fragments;
-import android.os.Bundle; import android.view.*; import android.widget.TextView;
-import androidx.annotation.NonNull; import androidx.fragment.app.Fragment;
+import android.os.Bundle; 
+import android.view.*; 
+import android.widget.TextView;
+import androidx.annotation.NonNull; 
+import androidx.fragment.app.Fragment;
 import com.google.android.material.button.MaterialButton;
-import ru.anton2319.vpnoverssh.data.singleton.StatusInfo; import ru.anton2319.vpnoverssh.data.utils.SSHConnectionProfileManager;
-import ru.anton2319.vpnoverssh.services.SshService; import android.content.Intent;
-import java.util.Timer; import java.util.TimerTask;
+import ru.anton2319.vpnoverssh.R;
+import ru.anton2319.vpnoverssh.data.singleton.StatusInfo; 
+import ru.anton2319.vpnoverssh.data.utils.SSHConnectionProfileManager;
+import ru.anton2319.vpnoverssh.services.SshService; 
+import android.content.Intent;
+import java.util.Timer; 
+import java.util.TimerTask;
 public class InicioFragment extends Fragment {
     TextView tv_bytes; MaterialButton btn; Timer timer;
     @Override public View onCreateView(@NonNull LayoutInflater inf, ViewGroup c, Bundle b){
@@ -14,20 +21,17 @@ public class InicioFragment extends Fragment {
         btn.setOnClickListener(view -> {
             if(StatusInfo.getInstance().isConnected()){
                 requireActivity().stopService(new Intent(requireActivity(), SshService.class));
-                btn.setIconResource(android.R.drawable.ic_media_pause);
             } else {
                 SSHConnectionProfileManager mgr = new SSHConnectionProfileManager(requireContext());
-                if(mgr.getProfiles().isEmpty()){
-                    tv_bytes.setText("Crea un perfil primero -> swipe a Registros");
+                if(mgr.loadProfiles().isEmpty()){
+                    tv_bytes.setText("Crea un perfil primero");
                     return;
                 }
-                // Usa el primer perfil
                 Intent i = new Intent(requireActivity(), SshService.class);
-                i.putExtra("uuid", mgr.getProfiles().get(0).getUuid().toString());
+                i.putExtra("uuid", mgr.loadProfiles().get(0).getUuid().toString());
                 requireActivity().startService(i);
             }
         });
-        // Actualiza bytes cada segundo
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask(){
             @Override public void run(){
@@ -35,11 +39,9 @@ public class InicioFragment extends Fragment {
                 getActivity().runOnUiThread(() -> {
                     StatusInfo si = StatusInfo.getInstance();
                     if(si.isConnected()){
-                        tv_bytes.setText(String.format("%.2f KB/s ↑ %.2f KB/s ↓", si.getUploadSpeed()/1024f, si.getDownloadSpeed()/1024f));
-                        btn.setText("CONECTADO");
+                        tv_bytes.setText(String.format("%.2f KB/s %.2f", si.getUploadSpeed()/1024f, si.getDownloadSpeed()/1024f));
                     } else {
-                        tv_bytes.setText(si.getStatusText().isEmpty()? "Desconectado - NANDOMX V5" : si.getStatusText());
-                        btn.setText("CONECTAR");
+                        tv_bytes.setText("Desconectado - NANDOMX V5");
                     }
                 });
             }
