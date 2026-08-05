56ed3037a3cfc09f24572a616b115b7a83878bfd
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 12:06:08 -0600

    FIX CRASH REAL - ViewPagerAdapter imports com.nandomx.v5.fragments - VERDE SIN CRASH

--- app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
@@ -1,42 +1,18 @@
 package ru.anton2319.vpnoverssh;
-import android.os.Bundle;
-import android.widget.Toast;
-import java.io.*;
 import com.nandomx.v5.R;
+import android.os.Bundle; 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager2.widget.ViewPager2; 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle b){
-        Thread.setDefaultUncaughtExceptionHandler((t, ex) -> {
-            try {
-                File dir = getExternalFilesDir(null);
-                if(dir==null) dir = getFilesDir();
-                File f = new File(dir, "crash_nandomx.txt");
-                PrintWriter pw = new PrintWriter(new FileWriter(f));
-                pw.println("=== CRASH NANDOMX V5 ===");
-                pw.println(ex.toString());
-                for(StackTraceElement el : ex.getStackTrace()) pw.println(" at " + el);
-                Throwable cause = ex.getCause();
-                if(cause!=null){ pw.println("Caused by: "+cause); for(StackTraceElement el : cause.getStackTrace()) pw.println(" at " + el); }
-                pw.close();
-            } catch (Exception e) { e.printStackTrace(); }
-            android.os.Process.killProcess(android.os.Process.myPid());
-            System.exit(2);
-        });
-
+    ViewPager2 vp; TabLayout tabs;
+    @Override protected void onCreate(Bundle b){ 
         super.onCreate(b);
-        try{
-            setContentView(R.layout.activity_main);
-            ViewPager2 vp = findViewById(R.id.viewPager);
-            TabLayout tabs = findViewById(R.id.tab_dots);
-            vp.setAdapter(new ViewPagerAdapter(this));
-            new TabLayoutMediator(tabs, vp, (tab,pos)->{}).attach();
-        } catch (Throwable e){
-            Toast.makeText(this, "CRASH: " + e.toString(), Toast.LENGTH_LONG).show();
-            throw e;
-        }
+        setContentView(R.layout.activity_main);
+        vp=findViewById(R.id.viewPager); 
+        tabs=findViewById(R.id.tab_dots);
+        vp.setAdapter(new ViewPagerAdapter(this));
+        new TabLayoutMediator(tabs, vp, (tab,pos)->{}).attach();
     }
 }
--- app/src/main/java/ru/anton2319/vpnoverssh/ViewPagerAdapter.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/ViewPagerAdapter.java
@@ -1,17 +1,17 @@
 package ru.anton2319.vpnoverssh;
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull; 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity; 
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import com.nandomx.v5.fragments.InicioFragment;
-import com.nandomx.v5.fragments.PerfilFragment;
-import com.nandomx.v5.fragments.SoporteFragment;
+import com.nandomx.v5.fragments.RegistrosFragment;
+import com.nandomx.v5.fragments.RawLogFragment;
 public class ViewPagerAdapter extends FragmentStateAdapter {
-    public ViewPagerAdapter(@NonNull FragmentActivity fa){ super(fa); }
-    @NonNull @Override public Fragment createFragment(int pos){
-        if(pos==1) return new PerfilFragment();
-        if(pos==2) return new SoporteFragment();
-        return new InicioFragment();
+    public ViewPagerAdapter(@NonNull FragmentActivity fa){super(fa);}
+    @NonNull @Override public Fragment createFragment(int p){ 
+        if(p==0) return new InicioFragment(); 
+        if(p==1) return new RegistrosFragment(); 
+        return new RawLogFragment(); 
     }
-    @Override public int getItemCount(){ return 3; }
+    @Override public int getItemCount(){return 3;}
 }
