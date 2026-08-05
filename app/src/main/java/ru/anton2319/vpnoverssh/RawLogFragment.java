47edb5adf8d8f28e4b8a717dbd08ac3ef7c55a90
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 03:16:58 -0600

    FIX RawLogFragment sin R

--- app/src/main/java/ru/anton2319/vpnoverssh/RawLogFragment.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/RawLogFragment.java
@@ -1,22 +1,7 @@
 package ru.anton2319.vpnoverssh;
-import com.nandomx.v5.R;
-import android.os.Bundle; import android.view.*; import android.widget.TextView;
-import androidx.annotation.NonNull; import androidx.fragment.app.Fragment;
-import ru.anton2319.vpnoverssh.data.singleton.StatusInfo;
-import java.util.Timer; import java.util.TimerTask;
+import android.os.Bundle; import android.view.LayoutInflater; import android.view.View; import android.view.ViewGroup; import android.widget.TextView; import androidx.annotation.NonNull; import androidx.annotation.Nullable; import androidx.fragment.app.Fragment;
 public class RawLogFragment extends Fragment {
-    TextView log; Timer timer;
-    @Override public View onCreateView(@NonNull LayoutInflater inf, ViewGroup c, Bundle b){
-        View v = inf.inflate(R.layout.fragment_raw, c, false);
-        log = v.findViewById(R.id.rawLogText);
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask(){
-            @Override public void run(){
-                if(getActivity()==null) return;
-                getActivity().runOnUiThread(() -> log.setText(StatusInfo.getInstance().getRawLog()));
-            }
-        },0,1000);
-        return v;
-    }
-    @Override public void onDestroyView(){ super.onDestroyView(); if(timer!=null) timer.cancel(); }
+ @Nullable @Override public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+  TextView tv = new TextView(getContext()); tv.setText("LOG NANDOMX"); tv.setTextSize(16); tv.setTextColor(0xFFFFFFFF); tv.setPadding(30,150,30,30); return tv;
+ }
 }
