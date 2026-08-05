4ef183aa8db7ec2ef844b13ba79dd7bffe0a75e1
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 03:16:22 -0600

    FIX CRASH RAIZ - import com.nandomx.v5.R eliminado, era el que sacaba la app

--- app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
@@ -1,12 +1,16 @@
 package ru.anton2319.vpnoverssh;
-import com.nandomx.v5.R;
-import android.os.Bundle; import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2; import com.google.android.material.tabs.TabLayout;
+import android.os.Bundle; 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2; 
+import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 public class MainActivity extends AppCompatActivity {
     ViewPager2 vp; TabLayout tabs;
-    @Override protected void onCreate(Bundle b){ super.onCreate(b); setContentView(R.layout.activity_main);
-        vp=findViewById(R.id.viewPager); tabs=findViewById(R.id.tab_dots);
+    @Override protected void onCreate(Bundle b){ 
+        super.onCreate(b); 
+        setContentView(R.layout.activity_main);
+        vp=findViewById(R.id.viewPager); 
+        tabs=findViewById(R.id.tab_dots);
         vp.setAdapter(new ViewPagerAdapter(this));
         new TabLayoutMediator(tabs, vp, (tab,pos)->{}).attach();
     }
