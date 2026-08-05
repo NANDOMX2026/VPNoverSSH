8fa8c6f65474326e56fa71d71763ca7ba85b1e95
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 03:20:17 -0600

    FIX R correcto ru.anton2319.vpnoverssh.R

--- app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
@@ -1,15 +1,16 @@
 package ru.anton2319.vpnoverssh;
-import android.os.Bundle; 
+import android.os.Bundle;
+import ru.anton2319.vpnoverssh.R;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2; 
+import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 public class MainActivity extends AppCompatActivity {
     ViewPager2 vp; TabLayout tabs;
-    @Override protected void onCreate(Bundle b){ 
-        super.onCreate(b); 
+    @Override protected void onCreate(Bundle b){
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
-        vp=findViewById(R.id.viewPager); 
+        vp=findViewById(R.id.viewPager);
         tabs=findViewById(R.id.tab_dots);
         vp.setAdapter(new ViewPagerAdapter(this));
         new TabLayoutMediator(tabs, vp, (tab,pos)->{}).attach();
