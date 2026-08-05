fa9da462952e41db6975889402bf3fe561c0266f
NANDOMX2026 <nandomx2026@github.com>
2026-08-05 03:03:21 -0600

    FIX import R

--- app/src/main/java/ru/anton2319/vpnoverssh/RegistrosFragment.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/RegistrosFragment.java
@@ -2,6 +2,6 @@ package ru.anton2319.vpnoverssh;
 import android.os.Bundle; import android.view.LayoutInflater; import android.view.View; import android.view.ViewGroup; import androidx.annotation.NonNull; import androidx.annotation.Nullable; import androidx.fragment.app.Fragment;
 public class RegistrosFragment extends Fragment {
  @Nullable @Override public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-  return inflater.inflate(R.layout.fragment_registros, container, false);
+  return inflater.inflate(ru.anton2319.vpnoverssh.R.layout.fragment_registros, container, false);
  }
 }
