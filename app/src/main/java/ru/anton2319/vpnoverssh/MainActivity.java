858d7156472ec00c522e5e8db747717b2a28383f
NANDOMX2026 <orlandolopezmendez1991@gmail.com>
2026-08-05 21:15:00 -0600

    Update MainActivity.java

--- app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
+++ app/src/main/java/ru/anton2319/vpnoverssh/MainActivity.java
@@ -1,6 +1,6 @@
-
 package ru.anton2319.vpnoverssh;
 
+import com.nandomx.v5.R;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(b);
         try {
             setContentView(R.layout.activity_main);
-            // Si el layout falla, muestra texto
         } catch (Exception e) {
             TextView tv = new TextView(this);
-            tv.setText("NANDOMX V5 - MainActivity OK\n\nError en layout: " + e.getMessage());
+            tv.setText("NANDOMX V5 - MainActivity OK\n\nLayout error: " + e.getMessage());
             tv.setPadding(50,300,50,50);
             setContentView(tv);
         }
